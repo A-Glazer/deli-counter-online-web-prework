d7304e1c121a72153864ef47e82ed13785361c23
A Glazer <avigayilglazer@gmail.com>
2019-01-08 18:38:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,15 +2,21 @@ katz_deli = []
 other_deli = ["Logan", "Avi", "Spencer"]
 another_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
 
+
 def line(katz_deli)
   if katz_deli.size == 0
     puts "The line is currently empty."
   else
     message = "The line is currently:"
+
     katz_deli.each_with_index {|other_deli, index| message += " #{index+1}. #{other_deli}"}
+
+    katz_deli.each_with_index {|ppl, index| message += " #{index+1}. #{ppl}"}
+
     puts message
   end
 end
+
 
 def take_a_number(katz_deli, other_deli)
   if katz_deli.size == 0
@@ -29,5 +35,14 @@ def now_serving(katz_deli)
   else
     num = other_deli.shift
     puts "Currently serving #{num}."
+
+def take_a_number(katz_deli, ppl)
+  if katz_deli.size == 0
+    puts katz_deli << ppl
+  elsif katz_deli.size > 0
+    katz_deli << ppl
+  else
+    puts "hi"
+
   end
 end
